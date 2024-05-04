332126d0c13020b8a390733a2b583209f68a7af6
Shernan Javier <shernanjavier@gmail.com>
2024-05-04 16:55:50 -0400

    Initialized Twilio Function

--- main.go
+++ main.go
@@ -1,13 +1,30 @@
 package main
 
-// import (
-// 	"net/http"
-// 	"strings"
+import (
+	"net/http"
 
-// 	"github.com/gin-gonic/gin"
-// 	"github.com/sfreiberg/gotwilio"
-// )
+	"github.com/gin-gonic/gin"
+	api "github.com/twilio/twilio-go/rest/api/v2010"
+)
 
 func main() {
 
+	router := gin.Default()
+
+	router.POST("/incoming-call", incomingCallHandler)
+
+	router.Run(":8080")
+}
+
+// Handles Twilio Webhook for incoming calls
+func incomingCallHandler(c *gin.Context) {
+
+	// Parses the incoming Twilio request
+
+	msg := &api.CreateMessageParams{}
+	err := c.Bind(&msg)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
 }

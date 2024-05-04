26350e33e66725326a82da046d85966962ee53bd
Shernan Javier <shernanjavier@gmail.com>
2024-05-04 18:14:42 -0400

    Completed a basic working phone answer

--- main.go
+++ main.go
@@ -3,12 +3,10 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
-	"github.com/twilio/twilio-go"
-	api "github.com/twilio/twilio-go/rest/api/v2010"
+	"github.com/twilio/twilio-go/twiml"
 )
 
 func loadEnv() {
@@ -27,7 +25,7 @@ func main() {
 
 	router := gin.Default()
 
-	router.POST("/incoming-call", incomingCallHandler)
+	router.POST("/answer", incomingCallHandler)
 
 	router.Run(":8080")
 }
@@ -37,34 +35,45 @@ func incomingCallHandler(c *gin.Context) {
 
 	// Parses the incoming Twilio request
 
-	var msg = &api.CreateMessageParams{}
-	err := c.Bind(&msg)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
+	// var msg = &api.CreateMessageParams{}
+	// err := c.Bind(&msg)
+	// if err != nil {
+	// 	c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	// 	return
+	// }
+
+	// // Test Message
+
+	// testMessage := "Hello, this is a test message from Mockcim."
+
+	// accountSid := os.Getenv("TWILIO_ACCOUNT_SID")
+	// authToken := os.Getenv("TWILIO_AUTH_TOKEN")
+	// phoneNumber := os.Getenv("TWILIO_PHONE_NUMBER")
+
+	// client := twilio.NewRestClientWithParams(twilio.ClientParams{
+	// 	Username: accountSid,
+	// 	Password: authToken,
+	// })
+
+	// params := &api.CreateMessageParams{}
+	// params.SetFrom(phoneNumber)
+	// params.SetBody(testMessage + "\n Shernan is also really cool and you should hire him if you're interested in hiring a software engineer.")
+
+	// // Send the message
+	// _, err = client.Api.CreateMessage(params)
+	// if err != nil {
+	// 	c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	// 	return
+	// }
+	say := &twiml.VoiceSay{
+		Message: "Hello, this is a test message from Mockcim. Shernan is also really cool and you should hire him if you're interested in hiring a software engineer.",
 	}
 
-	// Test Message
-
-	testMessage := "Hello, this is a test message from Mockcim."
-
-	accountSid := os.Getenv("TWILIO_ACCOUNT_SID")
-	authToken := os.Getenv("TWILIO_AUTH_TOKEN")
-	phoneNumber := os.Getenv("TWILIO_PHONE_NUMBER")
-
-	client := twilio.NewRestClientWithParams(twilio.ClientParams{
-		Username: accountSid,
-		Password: authToken,
-	})
-
-	params := &api.CreateMessageParams{}
-	params.SetFrom(phoneNumber)
-	params.SetBody(testMessage + "\n Shernan is also really cool and you should hire him if you're interested in hiring a software engineer.")
-
-	// Send the message
-	_, err = client.Api.CreateMessage(params)
+	twimlResult, err := twiml.Voice([]twiml.Element{say})
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
+		c.String(http.StatusInternalServerError, err.Error())
+	} else {
+		c.Header("Content-Type", "text/xml")
+		c.String(http.StatusOK, twimlResult)
 	}
 }

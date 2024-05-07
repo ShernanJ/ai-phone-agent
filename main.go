248c0a3423d6daf44cc94bc5804a8584fe222b6d
Shernan Javier <shernanjavier@gmail.com>
2024-05-07 08:47:59 -0400

    Reverted code for Proof of Concept

--- main.go
+++ main.go
@@ -2,22 +2,16 @@ package main
 
 import (
 	"context"
-	"errors"
-	"io"
 	"log"
 	"net/http"
 	"os"
-	"strings"
-	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"github.com/twilio/twilio-go/twiml"
 
 	cohere "github.com/cohere-ai/cohere-go/v2"
-	"github.com/cohere-ai/cohere-go/v2/client"
 	cohereclient "github.com/cohere-ai/cohere-go/v2/client"
-	"github.com/cohere-ai/cohere-go/v2/finetuning"
 )
 
 func loadEnv() {
@@ -28,122 +22,21 @@ func loadEnv() {
 	}
 }
 
-type MyReader struct {
-	io.Reader
-	name string
-}
-
-func (m *MyReader) Name() string {
-	return m.name
-}
 func main() {
 	// Load Environment Variables
 	loadEnv()
 
-	co := client.NewClient(client.WithToken(os.Getenv("COHERE_API_KEY")))
+	router := gin.Default()
 
-	// Read dataset file
-	datasetFile := "test.jsonl"
-	datasetContent, err := os.ReadFile(datasetFile)
-	if err != nil {
-		log.Fatalf("Failed to read dataset file: %v", err)
-	}
+	router.POST("/answer", incomingCallHandler)
+	router.POST("/handle-user-input", handleUserInput)
 
-	// Create dataset
-	resp, err := co.Datasets.Create(
-		context.TODO(),
-		&MyReader{Reader: strings.NewReader(string(datasetContent))},
-		&MyReader{Reader: strings.NewReader("")},
-		&cohere.DatasetsCreateRequest{
-			Name: "test-dataset",
-			Type: cohere.DatasetTypeSingleLabelClassificationFinetuneInput,
-		},
-	)
-	if err != nil {
-		log.Fatalf("Failed to create dataset: %v", err)
-	}
-	log.Printf("Dataset created: %+v", resp)
-
-	// Wait for dataset validation
-	for {
-		datasetStatus, err := co.Datasets.Get(context.TODO(), *resp.Id)
-		if err != nil {
-			log.Fatal("Error getting dataset status:", err)
-		}
-
-		if datasetStatus.Dataset.ValidationStatus == cohere.DatasetValidationStatusValidated {
-			break
-		}
-
-		log.Printf("Dataset is still processing. Waiting for validation...")
-		time.Sleep(5 * time.Second)
-	}
-
-	// Test question
-	question := "What car does Shernan drive?"
-
-	// Create a finetuned model
-	modelRes, err := co.Finetuning.CreateFinetunedModel(
-		context.TODO(),
-		&finetuning.FinetunedModel{
-			Name: "test-finetuned-model",
-			Settings: &finetuning.Settings{
-				DatasetId: *resp.Id,
-				BaseModel: &finetuning.BaseModel{
-					BaseType: finetuning.BaseTypeBaseTypeClassification,
-				},
-			},
-		},
-	)
-	if err != nil {
-		log.Fatalf("Failed to create finetuned model: %v", err)
-	}
-	log.Printf("Finetuned model created: %+v", modelRes.FinetunedModel)
-
-	// Chat with the finetuned model
-	testRes, err := co.ChatStream(
-		context.TODO(),
-		&cohere.ChatStreamRequest{
-			Model:   modelRes.FinetunedModel.Id,
-			Message: question,
-		},
-	)
-
-	if err != nil {
-		log.Fatalf("Failed to start chat stream: %v", err)
-	}
-
-	defer testRes.Close()
-
-	for {
-		message, err := testRes.Recv()
-
-		if errors.Is(err, io.EOF) {
-			// An io.EOF error means the server is done sending messages
-			// and should be treated as a success.
-			break
-		}
-
-		if err != nil {
-			log.Fatal("Error receiving message:", err)
-		}
-
-		if message.TextGeneration != nil {
-			log.Printf("Received message: %+v", message.TextGeneration)
-		}
-	}
-
-	// router := gin.Default()
-
-	// router.POST("/answer", incomingCallHandler)
-	// router.POST("/handle-user-input", handleUserInput)
-
-	// router.Run(":8080")
+	router.Run(":8080")
 }
 
 // Handles Twilio Webhook for incoming calls
 func incomingCallHandler(c *gin.Context) {
-	msg := "Welcome! This is a test call, Try to ask me questions and I will try to answer them. Example, ask me how the weather is like in your location."
+	msg := "Welcome! This is a test app, ask me questions related to " + os.Getenv("COMPANY_NAME")
 
 	gather := &twiml.VoiceGather{
 		Input:         "speech",
@@ -171,31 +64,10 @@ func handleUserInput(c *gin.Context) {
 
 	client := cohereclient.NewClient(cohereclient.WithToken(os.Getenv("COHERE_API_KEY")))
 
-	// Temporary Dataset
-	dataset, err := client.Datasets.Create(
-		context.TODO(),
-		&MyReader{Reader: strings.NewReader(""), name: "test.jsonl"},
-		&MyReader{Reader: strings.NewReader(""), name: "a.jsonl"},
-		&cohere.DatasetsCreateRequest{
-			Name: "prompt-completion-dataset",
-			Type: cohere.DatasetTypeEmbedResult,
-		},
-	)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	log.Printf("%+v", dataset)
-
-	datasetResult, err := client.Datasets.Get(context.TODO(), "prompt-completion-dataset")
-
-	log.Printf("Dataset: %v", datasetResult)
-
 	response, err := client.Chat(
 		context.TODO(),
 		&cohere.ChatRequest{
-			Message: userInput,
+			Message: os.Getenv("SEED_MESSAGE") + userInput,
 		},
 	)
 
